7f81c90beb10391e2702073c118b8776082c212f
n3ndo <ferdinandof3lixbagnale@gmail.com>
2023-11-17 18:48:03 +0100

    fixed index.html and script.js

--- js/script.js
+++ js/script.js
@@ -2,4 +2,4 @@ let nome = prompt("Inserisci il tuo nome");
 let cognome = prompt("Inserisci il tuo cognome");
 let colore_preferito = prompt("Inserisci il tuo colore preferito");
 
-document.getElementById("password").innerHTML = nome + cognome + colore_preferito + document.getElementById("password").innerHTML ;
+document.getElementById("password").innerHTML = nome + cognome + colore_preferito + 23 ;
