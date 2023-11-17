3470f3ffb49a3b3dc5aeb486686dce88c4e813f0
n3ndo <ferdinandof3lixbagnale@gmail.com>
2023-11-17 16:44:32 +0100

    end exercise

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let nome = prompt("Inserisci il tuo nome");
+let cognome = prompt("Inserisci il tuo cognome");
+let colore_preferito = prompt("Inserisci il tuo colore preferito");
+
+document.getElementById("password").innerHTML = nome + cognome + colore_preferito + document.getElementById("password").innerHTML ;
